557d6a7af4b0eaffc68711a30fe1e2fdcab08467
Keerthivasan SS <keerthiss73@gmail.com>
2025-12-04 14:12:36 +0530

    Add files via upload

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String Name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		float mobileNumber = 9876543210f;
+		long pinCode = 56001;
+		short luckyNumber = 7;
+		
+		System.out.println(Name);
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(gender);
+		System.out.println(acceptTerms);
+		System.out.println(rating);
+		System.out.println(mobileNumber);
+		System.out.println(pinCode);
+		System.out.println(luckyNumber);
+
+	}
+
+}
---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// Data Type- Primitive and Non-Primitive
+					//Primitive- int, char, boolean, float
+					//Non-Primitive- String, Array
+				
+				//syntax for storing a value
+				//Datatype var.name = value;
+				
+				int month = 12;
+				String monthName = "December" ;
+				boolean currentMonth = true;
+				char day = 'w';
+				float decimal = 1.927287f;
+				
+				System.out.println(month);
+				System.out.println(monthName);
+				System.out.println(currentMonth);
+				System.out.println(day + " " +decimal); //(Cocordinate - combining more than one output in the same line using variable name1 + " " + Variable name2)
+				//System.out.println(decimal);
+
+	}
+
+}
